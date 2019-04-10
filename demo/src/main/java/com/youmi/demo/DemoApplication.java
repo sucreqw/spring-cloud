9604b3a1ef5486417a9f696201fa2cf756efb217
sucre <sucre@youmi.com>
2019-04-10 13:39:35 +0800

    spring-cloud

--- demo/src/main/java/com/youmi/demo/DemoApplication.java
+++ demo/src/main/java/com/youmi/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.youmi.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,8 +11,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@ComponentScan(basePackages = {"com.youmi.demo.controller","com.youmi.demo.domain","com.youmi.demo.service"})
+@EnableCaching
+@ComponentScan(basePackages = {"com.youmi.demo.controller","com.youmi.demo.domain","com.youmi.demo.service","com.youmi.demo.config"})
 @MapperScan("com.youmi.demo.dao")
+
 public class DemoApplication {
 
     public static void main(String[] args) {
--- demo/src/main/java/com/youmi/demo/controller/Controllers.java
+++ demo/src/main/java/com/youmi/demo/controller/Controllers.java
@@ -7,6 +7,7 @@ import com.youmi.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author sucre chen 906509023@qq.com
@@ -32,6 +34,8 @@ public class Controllers {
     OpenFeign openFeign;
     @Autowired
     UserService userService;
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @GetMapping("/demo")
     public String feign() {
@@ -44,19 +48,33 @@ public class Controllers {
         return "8084";
     }
 
-    @GetMapping(value="/getuser" ,produces = { "application/json;charset=UTF-8" })
+    @GetMapping(value = "/getuser", produces = {"application/json;charset=UTF-8"})
     public User testSQL(int id) {
-        User user=userService.getByUserId(id);
+        User user = userService.getByUserId(id);
 
         return user;
     }
 
-    @GetMapping(value="/searchuser",produces = { "application/json;charset=UTF-8" })
-    public User search(String nick ) {
+    @GetMapping(value = "/searchuser", produces = {"application/json;charset=UTF-8"})
+    public User search(String nick) {
 
-        List<User> user=new ArrayList<>();
-        user=userService.search(nick);
+        List<User> user = new ArrayList<>();
+        user = userService.search(nick);
 
         return user.get(0);
+    }
+
+    @GetMapping("/setredis")
+    public String setredis(String data) {
+        // 最后参数设置时间单位，第三个设置的是时间。（2天后过期）
+        redisTemplate.opsForValue().set("test", data, 2, TimeUnit.DAYS);
+        return "redis set:" + data;
+    }
+
+    @GetMapping("/getredis")
+    public String getredis() {
+        // 最后参数设置时间单位，第三个设置的是时间。（2天后过期）
+
+        return redisTemplate.opsForValue().get("test").toString();
     }
 }
